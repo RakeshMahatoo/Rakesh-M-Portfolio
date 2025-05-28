16840f6c87ac1bd970381eb2425b0b443b1ce9a6
RAKESH MAHATO <RakeshMahatoo@example.com>
2025-05-29 00:41:34 +0530

    update Resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1YLK5hCq_NDBHCi4RCWcCCAajDKbtbP1i/view?usp=sharing"
+            href="https://drive.google.com/file/d/13SwEtxZmF_0WQTGwBb9l_-w43SvIMXpJ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
